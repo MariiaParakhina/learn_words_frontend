e42a089ba5cab0def999e1fd5f62c7c26e47b031
MariiaParakhina <parakhinam185@gmail.com>
2025-01-12 17:47:39 +0100

    make collections page cleaner

--- memorize-words/src/views/Tab1Page.vue
+++ memorize-words/src/views/Tab1Page.vue
@@ -15,21 +15,13 @@
         <ion-card v-for="collection in collections" :key="collection.id" @click="handleClick(collection.id)">
           <ion-card-header class="card-header">
             <ion-card-title>{{ collection.name }}</ion-card-title>
-
           </ion-card-header>
           <ion-card-content>
             <p>{{ collection.description }}</p>
             <div class="circular-progress">
               <svg viewBox="0 0 36 36" class="circular-chart">
-                <path class="circle-bg"
-                      d="M18 2.0845
-                         a 15.9155 15.9155 0 0 1 0 31.831
-                         a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                <path class="circle"
-                      :stroke-dasharray="GetProgressPercentage(collection.status) + ', 100'"
-                      d="M18 2.0845
-                         a 15.9155 15.9155 0 0 1 0 31.831
-                         a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                <path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                <path class="circle" :stroke-dasharray="GetProgressPercentage(collection.status) + ', 100'" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/>
                 <text x="18" y="20.35" class="percentage">{{ GetProgressPercentage(collection.status) }}%</text>
               </svg>
               <div class="status-text">
@@ -54,25 +46,11 @@
 </template>
 
 <script setup lang="ts">
-import {ref, onMounted, watch, watchEffect} from 'vue';
-import {useRouter, useRoute} from 'vue-router';
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
-  IonFab,
-  IonFabButton,
-  IonIcon
-} from '@ionic/vue';
+import { ref, onMounted, watchEffect } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import { addOutline } from 'ionicons/icons';
 import api from '@/services/api';
-import {addOutline} from 'ionicons/icons';
-import {GetProgressPercentage} from '../services/timingService';
+import { GetProgressPercentage } from '../services/timingService';
 
 const router = useRouter();
 const route = useRoute();
@@ -111,27 +89,14 @@ const remainingTime = (updatedAt: string, status: string) => {
   const now = new Date();
   const lastUpdated = new Date(updatedAt);
   const differenceInMs = now.getTime() - lastUpdated.getTime();
-  let requiredTimeInSeconds = 0;
-
-  switch (status) {
-    case 'ONE_HOUR':
-      requiredTimeInSeconds = 3600;
-      break;
-    case 'ONE_DAY':
-      requiredTimeInSeconds = 86400;
-      break;
-    case 'TWO_DAYS':
-      requiredTimeInSeconds = 172800;
-      break;
-    case 'FIVE_DAYS':
-      requiredTimeInSeconds = 432000;
-      break;
-    case 'ONE_MONTH':
-      requiredTimeInSeconds = 2592000;
-      break;
-  }
-
-  return Math.max(requiredTimeInSeconds - Math.floor(differenceInMs / 1000), 0);
+  const statusDurations = {
+    'ONE_HOUR': 3600,
+    'ONE_DAY': 86400,
+    'TWO_DAYS': 172800,
+    'FIVE_DAYS': 432000,
+    'ONE_MONTH': 2592000
+  };
+  return Math.max(statusDurations[status] - Math.floor(differenceInMs / 1000), 0);
 };
 
 const remainingTimeText = (updatedAt: string, status: string) => {
@@ -149,36 +114,34 @@ const updateCountdowns = () => {
   }));
 };
 </script>
+
 <style scoped>
 .collections-grid {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 16px;
   padding: 16px;
-  background-color: #1B263B; /* Main color */
+  background-color: #1B263B;
 }
 
 ion-header, ion-content, ion-toolbar, ion-title, ion-header, .collections-grid {
-  background-color: transparent; /* Main color */
+  background-color: transparent;
 }
 
 ion-card {
-  background-color: rgba(27, 38, 59, 0.8); /* Slightly transparent card background */
-  color: white; /* Text color */
+  background-color: rgba(27, 38, 59, 0.8);
+  color: white;
 }
 
 ion-card-header {
   background-color: transparent;
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
   padding: 8px 16px;
 }
 
 ion-card-content {
   display: flex;
-  flex-direction: row;
-  width: 100%;
   justify-content: space-between;
   align-items: center;
 }
@@ -189,17 +152,17 @@ ion-card-content {
 }
 
 ion-card-title {
-  color: white; /* Card title text color */
-  margin: 0; /* Remove margin to reduce space */
+  color: white;
+  margin: 0;
 }
 
 ion-card-content p {
-  color: white; /* Card content text color */
+  color: white;
 }
 
 ion-fab-button {
-  --background: #1B263B; /* FAB button background color */
-  --color: white; /* FAB button icon color */
+  --background: #1B263B;
+  --color: white;
 }
 
 .circular-progress {
@@ -217,13 +180,13 @@ ion-fab-button {
 .circle-bg {
   fill: none;
   stroke: #e0e0e0;
-  stroke-width: 2.8; /* Thinner background circle */
+  stroke-width: 2.8;
 }
 
 .circle {
   fill: none;
   stroke: #1B263B;
-  stroke-width: 2; /* Thinner progress circle */
+  stroke-width: 2;
   stroke-linecap: round;
   transition: stroke-dasharray 0.6s ease 0s;
 }
